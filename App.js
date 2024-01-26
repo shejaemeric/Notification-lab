af1d86bcf162798e2d76b9da831d006e6716d956
shejaemeric051@gmail.com <shejaemeric2@gmail.com>
2024-01-26 15:18:03 +0200

    refactor

--- App.js
+++ App.js
@@ -16,6 +16,34 @@ export default function App() {
     }),
   });
 
+  async function registerForPushNotificationsAsync() {
+    if (Platform.OS === "android") {
+      await Notifications.setNotificationChannelAsync("default", {
+        name: "default",
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: "#FF231F7C",
+      });
+    }
+
+    if (Device.isDevice) {
+      const { status: existingStatus } =
+        await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== "granted") {
+        alert("Failed to get push token for push notification!");
+        return;
+      }
+      await Notifications.getExpoPushTokenAsync();
+    } else {
+      alert("Must use physical device for Push Notifications");
+    }
+  }
+
   const sendNotification = async () => {
     await Notifications.scheduleNotificationAsync({
       content: {
